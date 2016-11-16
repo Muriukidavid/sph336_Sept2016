e12b9fe235fc4fd2ac417ff6bf325bf51972d622
mathew1993 <ngangamathew9@gmail.com>
2016-11-16 11:29:27 +0300

    This is 2by4_decoder please add
    In my 2by4_decoder i used SystemC code to design it. As well i have included my circuit, M.O.C,truth table and readme file.

---
+++ mathew_nganga/decoder_2by4/driver4.h
@@ -0,0 +1,38 @@
+/*
+ * driver.h
+ *
+ *  Created on: Oct 28, 2016
+ *      Author: mathew
+ */
+
+#ifndef DRIVER4_H_
+#define DRIVER4_H_
+
+#include<systemc>
+
+SC_MODULE(driver){
+sc_out<bool> d_a,d_b;
+
+SC_CTOR(driver){
+	SC_THREAD(drive);
+	
+}
+
+void drive(void){
+	while(1){
+		d_a=0;
+		d_b=0;
+		wait(5,SC_NS);
+		d_a=0;
+		d_b=1;
+		wait(5,SC_NS);
+		d_a=1;
+		d_b=0;
+		wait(5,SC_NS);
+		d_a=1;
+		d_b=1;
+		wait(5,SC_NS);
+	}
+}
+};
+#endif /* DRIVER_H_ */
