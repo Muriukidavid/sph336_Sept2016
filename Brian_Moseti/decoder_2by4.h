f00a6f054f16cadb4b96564c364726f1a6616b92
Moseti Brian <b.moseti20@gmail.com>
2016-11-16 15:16:48 +0300

    Brian Moseti decoder two_by_four

---
+++ Brian_Moseti/decoder_2by4.h
@@ -0,0 +1,49 @@
+#include "decoder_1by2.h"
+
+SC_MODULE (two_by_four){
+
+	sc_in<bool> r,s;
+	sc_out<bool>e,f,g,h;
+	sc_signal<bool> s1,s2,s3,s4;
+
+	decoder *dec1_ptr, *dec2_ptr;
+
+SC_CTOR (two_by_four){
+	
+	dec1_ptr = new decoder("dec1");
+		dec1_ptr -> a (r);
+		dec1_ptr -> b(s1);
+		dec1_ptr -> c(s2);
+	dec2_ptr = new decoder("dec2");
+		dec2_ptr -> a (s);
+		dec2_ptr -> b (s3);
+		dec2_ptr -> c (s4);
+
+SC_METHOD (prc_one);
+	sensitive<<s1<<s3;
+SC_METHOD(prc_two);
+	sensitive<<s1<<s4;
+SC_METHOD(prc_three);
+	sensitive<<s2<<s3;
+SC_METHOD(prc_four);
+	sensitive<<s2<<s4;
+
+}
+//deconstructor
+~two_by_four(){
+
+}
+void prc_one(){
+	e = s1 && s3;
+}
+void prc_two(){
+	f = s1 && s4;
+}
+void prc_three(){
+	g = s2 && s3;
+}
+void prc_four(){
+	h = s2 && s4;
+}
+
+};
